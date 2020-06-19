aea5f707134fdb6c106a31558303d704601d0831
jordones <jsmith72@uoguelph.ca>
2020-06-18 21:57:44 -0400

    fix(firebase import): fix import of firebase from config

--- services/firebase.js
+++ services/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 import "firebase/auth";
 import "firebase/firestore";
 
-import firebaseDevConfig from './firebaseConfig';
+import {firebaseDevConfig} from './firebaseConfig';
 
 const app = firebase.initializeApp(firebaseDevConfig);
 export const auth = app.auth();
